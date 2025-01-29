9dc76590d526458503d03029bd871825d750bad0
jassouline <jordan.assouline@hotmail.fr>
2025-01-29 09:20:31 +0100

    Forgot to change the version in setup.py and __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jasapp",
-    version="0.1.3",
+    version="0.1.4",
     description="A tool for linting Dockerfiles & Kubernetes manifests with scoring capabilities.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
