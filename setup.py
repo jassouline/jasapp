f033375e9e0cb4ec1726971c6d3d06599da471b7
jassouline <jordan.assouline@hotmail.fr>
2025-02-17 10:53:25 +0100

    Add the correct version into setup.py file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jasapp",
-    version="0.1.4.1",
+    version="0.2.0",
     description="A tool for linting Dockerfiles & Kubernetes manifests with scoring capabilities.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
